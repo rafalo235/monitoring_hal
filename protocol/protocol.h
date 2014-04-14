563ff2f3bcbf10049c21c10bc5b2eefcde287321
Rafał Olejniczak <rafalo235@gmail.com>
2014-04-14 10:03:14 +0200

    Protokół v.0.1.1

--- protocol/protocol.h
+++ protocol/protocol.h
@@ -11,28 +11,28 @@ namespace monitoring_hal {
 	- sensor_id - identyfikator czujnika, czy 255 wystarczy?. 0xFF - będzie oznaczał sam koncentrator.
 	- time - znacznik czasowy. time_t wg. dokumentacji może być nieprzenośny, jakiś inny typ? */
 
-typedef struct {
+struct identifier {
 	uint8_t	sensor_id;
 	time_t		time; 
-} identifier;
+};
 
 
 /* Wartość zczytana przez koncentrator */
 
-typedef struct {
+struct single_value {
 	identifier		id;
 	uint32_t		value;
-} single_value;
+};
 
 
 /* Za pomocą control_reponse koncentrator będzie zgłaszał błędy i wysyłał wiadomości zwrotne dotyczące konfiguracji. Pola:
 	- id - Identyfikator określa czujnik (koncentrator ma zarezerwowany sensor_id=0xFF)
 	- code - kod błędu/wiadomości zwrotnej */
 
-typedef struct {
+struct control_response {
 	identifier		id;
 	uint8_t		code;
-} control_response;
+};
 
 
 /* Wiadomość od koncentratora do serwera.
@@ -41,13 +41,13 @@ typedef struct {
 	- values - wartości zczytane wraz z identyfikatorami czujników i znacznikami czasowymi
 	- control_responses - informacje zwrotne o wykonanej konfiguracji urządzenia, wiadomości o problemach technicznych */
 
-typedef struct {
+struct request_message {
 	uint8_t		values_amount;
 	uint8_t		cr_amount;
 	single_value		values[];
 	control_response	control_responses[];
 	int8_t			checksum;
-} request_message;
+};
 
 
 /* Wiadomość zwrotna od serwera do koncentratora
@@ -55,12 +55,12 @@ typedef struct {
 	- ids - potwierdzenie odebrania przesłanych wartości zczytanych (jedynie ich identyfikatory). Po odebraniu danego identyfikatora koncentrator może usunąć dany wpis przechowywany w buforze
 	- command - rozkaz przekonfigurowania koncentratora */
 
-typedef struct {
+struct response_message {
 	uint16_t		ids_amount;
 	identifier		ids[];
 	uint8_t		command;
 	int8_t			checksum;
-} response_message;
+};
 
 }
 
