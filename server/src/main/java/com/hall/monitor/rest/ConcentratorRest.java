048b922bd21e35ca00a3b43c81b439477a354d5b
iblis <marcin.serwach@gmail.com>
2014-05-15 17:15:30 +0200

    init cd 4

--- server/src/main/java/com/hall/monitor/rest/ConcentratorRest.java
+++ server/src/main/java/com/hall/monitor/rest/ConcentratorRest.java
@@ -1,4 +1,4 @@
-package com.hall.monitoring.rest;
+package com.hall.monitor.rest;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
--- server/src/test/java/com/hall/monitor/ConcentratorRestTest.java
+++ server/src/test/java/com/hall/monitor/ConcentratorRestTest.java
@@ -1,4 +1,4 @@
-package com.monitoring.hall;
+package com.hall.monitor;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
