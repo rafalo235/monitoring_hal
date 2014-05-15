e7193b56253df1fbe8cc16d5bb50031894c7fc11
iblis <marcin.serwach@gmail.com>
2014-05-15 16:50:39 +0200

    struktura

---
+++ server/src/main/java/com/hall/monitoring/rest/ConcentratorRest.java
@@ -0,0 +1,28 @@
+package com.hall.monitoring.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+
+@Path("/concentrator")
+public class ConcentratorRest {
+
+	
+
+	@POST
+	@Path("/post")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createTrackInJSON(byte bytes[]) {
+
+		String result = "working";
+		return Response.status(201).entity(result).build();
+		
+	}
+	
+}
--- server/src/test/java/com/hall/monitoring/client/ConcentratorRestTest.java
+++ server/src/test/java/com/hall/monitoring/client/ConcentratorRestTest.java
@@ -1,30 +1,32 @@
-package com.mkyong.client;
+package com.monitoring.hall;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-public class JerseyClientGet {
+public class ConcentratorRestTest {
 
 	public static void main(String[] args) {
+/*
 		try {
 
 			Client client = Client.create();
 
 			WebResource webResource = client
-					.resource("http://localhost:8080/RESTfulExample/rest/json/metallica/get");
+					.resource("http://localhost:8080/RESTfulExample/rest/json/metallica/post");
 
-			ClientResponse response = webResource.accept("application/json")
-					.get(ClientResponse.class);
+			String input = "{\"singer\":\"Metallica\",\"title\":\"Fade To Black\"}";
 
-			if (response.getStatus() != 200) {
+			ClientResponse response = webResource.type("application/json")
+					.post(ClientResponse.class, input);
+
+			if (response.getStatus() != 201) {
 				throw new RuntimeException("Failed : HTTP error code : "
 						+ response.getStatus());
 			}
 
-			String output = response.getEntity(String.class);
-
 			System.out.println("Output from Server .... \n");
+			String output = response.getEntity(String.class);
 			System.out.println(output);
 
 		} catch (Exception e) {
@@ -32,7 +34,7 @@ public class JerseyClientGet {
 			e.printStackTrace();
 
 		}
-
+*/
 	}
 
 }
