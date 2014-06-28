0aae2ed887f914a775d6088afa09f574c965e32d
Rafał Olejniczak <rafalo235@gmail.com>
2014-06-28 22:43:32 +0200

    Naprawione obliczanie CRC16 na koncentratorze; ważne obliczanie CRC16 na big endian; do zrobienia generowanie dzielnika

--- concentrator/Concentrator/util/Cryptography.cpp
+++ concentrator/Concentrator/util/Cryptography.cpp
@@ -4,33 +4,34 @@ namespace NUtil {
   uint16_t CCryptography::crc16(const char* data, int dataSize)
   {
     uint16_t crc;
-    int bufferLength = dataSize;
     int pointer = dataSize * 8;
-    char divisor[3] = { 0x8E, 0xEF, 0x80 }; //TODO
-    char *copy = (char*) calloc(dataSize + sizeof(uint16_t) + 1, 1);
+    char divisor[3] = { 0xFE, 0xEF, 0x80 }; //TODO
+    char *copy = (char*) calloc(dataSize, 1);
     char tmp;
+
     memcpy(copy, data, dataSize);
 
     do {
         for (int i = 0 ; i < 8 ; i++ ) {
+            //print(copy, bufferLength);
             if (copy[0] & 0x80) { // jeżeli najstarszy bit jest 1 oblicz XOR
                 copy[0] ^= divisor[0];
                 copy[1] ^= divisor[1];
                 tmp = copy[2];
                 copy[2] ^= divisor[2];
                 copy[2] &= 0x80;
-                copy[2] |= tmp & 0x78;
+                copy[2] |= tmp & 0x7F;
             }
-            shift(copy, bufferLength); // przesuń bity danych o 1
+            shift(copy, dataSize); // przesuń bity danych o 1
             pointer--;
         }
-        bufferLength--;
     } while (pointer > 0);
 
     memcpy(&crc, copy, sizeof(uint16_t));
 
+    crc = htobe16(crc);
+
     free(copy);
-    free(divisor);
     return crc;
   }
 
@@ -40,7 +41,7 @@ namespace NUtil {
         if (data[i] & 0x80) {
             data[i - 1] |= 0x01;
         }
-        data[i] = data[i] < 1;
+        data[i] = data[i] << 1;
     }
   }
 }
