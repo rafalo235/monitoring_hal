853d7b3a7c0f92d9573f4c20c64675898ee11370
iblis <marcin.serwach@gmail.com>
2014-07-01 09:52:59 +0200

    poprawiony blad z nazwa kolumny poprawiony blad dzielenia przez 0 gdy nie ma danych w bazie dla hali

--- MonitoringAdminPanel/src/main/java/com/monitoring/hall/beans/Hall.java
+++ MonitoringAdminPanel/src/main/java/com/monitoring/hall/beans/Hall.java
@@ -24,7 +24,7 @@ public class Hall {
 	@Column(nullable = false, name = "hallName")
 	private String hallName;
 
-	@Column(nullable = false, name = "hallAddress")
+	@Column(nullable = false, name = "address")
 	private String hallAddress;
 
 	@ManyToOne
